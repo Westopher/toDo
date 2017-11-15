2853c2961ff36ff515ec9d78bc7e225585973d5c
West Kraemer <kraemerwest@gmail.com>
2017-11-15 08:46:16 -0500

    add ability to click on complete button and remove at index path, then reload the data and send user back to table view controller.

--- toDo/CompleteToDoViewController.swift
+++ toDo/CompleteToDoViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class CompleteToDoViewController: UIViewController {
 
+    var previousVC = ToDoTableViewController()
+    var selectedToDo = ToDo()
+    
     @IBOutlet var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = selectedToDo.name
     }
 
     
     @IBAction func completeTapped(_ sender: Any) {
+        var index = 0
+        
+        for toDo in previousVC.toDos {
+            if toDo.name == selectedToDo.name {
+                previousVC.toDos.remove(at: index)
+                previousVC.tableView.reloadData()
+                navigationController?.popViewController(animated: true)
+                break
+            }
+            index += 1
+        }
+        
     }
     
 
